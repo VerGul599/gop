70ec4ffbc8907d34181d4392e2f0a8148d81f3df
Вероника <@WIN-BBNSI5FE1UN>
2024-03-10 21:30:31 +0500

    Добавьте файлы проекта.

---
+++ gop/gop.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int sum = 0;
+    int count = 0;
+
+    for (int i = 1; i <= 1000; i++) 
+    {
+        sum += i;
+        count++;
+    }
+
+    double average = static_cast<double>(sum) / count;
+
+    std::cout << "Среднее арифметическое всех целых чисел от 1 до 1000 равно: " << average ;
+
+    return 0;
+}
